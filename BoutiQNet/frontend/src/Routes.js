7f3f0914ff7f86fa1f96c13f94e944f1239146ff
skander kefi <94742678+Skandeerkefi@users.noreply.github.com>
2024-02-14 23:23:39 +0000

    first commit

---
+++ BoutiQNet/frontend/src/Routes.js
@@ -0,0 +1,4 @@
+import LoginPage from "../src/pages/Login.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import ActivationPage from "./pages/ActivationPage.jsx";
+export { LoginPage, SignupPage, ActivationPage };
